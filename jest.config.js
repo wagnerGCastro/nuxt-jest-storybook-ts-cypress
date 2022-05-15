d721d0c6b94e7476f4adac687792e9c13dde7ebe
Wagner Castro <wagner_gcastro@hotmail.com>
2022-05-15 07:02:12 -0300

    #01 - Initial commit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  testMatch: ['<rootDir>/__tests__/**/*.{spec,test}.{js,ts}'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.vue',
+    '<rootDir>/src/pages/**/*.vue',
+  ],
+  testEnvironment: 'jsdom',
+}
