474d74c8e0a3e1fcd539a7d7b14789c38eff52f6
Wagner Castro <wagner_gcastro@hotmail.com>
2022-05-20 07:32:24 -0300

    feat(cypress): #05- added cypress and configuration, performed first test

---
+++ cypress/integration/1-examples/example.ts
@@ -0,0 +1,18 @@
+// load type definitions from Cypress module
+// <reference types="cypress" />
+
+describe('Sample tests', () => {
+  it('Visits index page', () => {
+    cy.visit('/')
+    cy.contains('h1', 'NUXT + TypeScript + Vuetify')
+  })
+
+  it('increase counter', () => {
+    cy.viewport(1280, 1080)
+
+    cy.get('[type="text"]').type('10')
+    cy.get('[type="button"]').click({ multiple: true })
+
+    cy.contains('.v-alert.success div', '25')
+  })
+})
