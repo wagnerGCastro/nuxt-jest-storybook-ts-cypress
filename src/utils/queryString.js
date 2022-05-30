4e5d029e3be10d156ec732f2e4fbc16dabe4d445
Wagner Castro <wagner_gcastro@hotmail.com>
2022-05-30 07:37:09 -0300

    feat(test) #09 Library Cart and to implement test

--- src/utils/queryString.js
+++ src/utils/queryString.js
@@ -1,24 +1,26 @@
 const keyValueToString = ([key, value]) => {
   if (typeof value === 'object' && !Array.isArray(value)) {
-    throw new TypeError('Please check yout params');
+    throw new TypeError('Please check yout params')
   }
-  return `${key}=${value}`;
-};
+  return `${key}=${value}`
+}
 
 export function queryString(obj) {
-  return Object.entries(obj).map(keyValueToString).join('&');
+  return Object.entries(obj).map(keyValueToString).join('&')
 }
 
 export function parse(string) {
   return Object.fromEntries(
-    string.split('&').map(item => {
-      let [key, value] = item.split('=');
+    string.split('&').map((item) => {
+      /* eslint-disable */
+      let [key, value] = item.split('=')
 
       if (value?.indexOf(',') > -1) {
-        value = value.split(',');
+        value = value.split(',')
       }
 
-      return [key, value];
-    }),
-  );
+      return [key, value]
+      /* eslint-enable */
+    })
+  )
 }
